fda1ae9d80386f967a6426cb8ce87b7ddf2056f2
Tristan Schwarz <schwarz.tristans@gmail.com>
2024-02-18 17:40:14 +0100

    Replace index.html with index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html lang="de">
+    <head>
+        <meta name="author" content="Tristan Schwarz">
+        <meta name="description" content="Online Notizblock">
+        <meta name="keywords" content="noteblock, online noteblock, Notizblock, Online Notizblock">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>NOTEBLOCK</title>
+    </head>
+    <body>
+        <h1>NOTEBLOCK</h1>
+
+        <?php
+            echo "Das ist ein Text aus PHP";
+        ?>
+
+        <script>
+            console.log('noteblock js script part');
+        </script>
+    
+    </body>
+</html>
