ba8f6c470c062a9f7f4c09e0502dd6b5a1a15509
Tristan Schwarz <schwarz.tristans@gmail.com>
2024-02-19 18:31:41 +0100

    LocalStorageTest

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 function displayInputNote(event) {
     if (event.key == 'Enter') {
-        var inputNoteValue = event.target.value;
-        console.log('Wert der eingegeben wurde', inputNoteValue);
+        const inputNoteValue = event.target.value;
+        insertIntoTable(inputNoteValue);
+        let loadedStorageIndex = getLocalStorageLastIndex();
 
-        var createTableNoteContainer = '<table><tr><td>' + inputNoteValue + '</td></tr></table>';
-        // var createTableNoteContainer = 'test';
-        console.log(createTableNoteContainer);
-
-        document.getElementById('noteContainer').innerHTML = createTableNoteContainer;
+        if (loadedStorageIndex === undefined) {
+            loadedStorageIndex = 1;
+        }
+        
+        localStorage.setItem(loadedStorageIndex, inputNoteValue);
+        document.getElementById('inputNote').value = '';
     }
 }
+
+function loadLocalStorage() {
+    console.log('load Local Storage...');    
+    
+    for (let localStorageIndex=0; localStorageIndex<localStorage.length; localStorageIndex++) {
+        let storageIndex = localStorage.key(localStorageIndex);
+        console.log(storageIndex + ':' + localStorage.getItem(storageIndex));
+        console.log('storageIndex: ' + storageIndex);
+        insertIntoTable(localStorage.getItem(storageIndex));
+    }
+}
+
+function insertIntoTable(insertTable) {
+    console.log('load Value:' + insertTable + ' into Table...');
+    const createTableNoteContainer = '<table><tr><td>' + insertTable + '</td></tr></table>';
+    document.getElementById('noteContainer').innerHTML += createTableNoteContainer;
+}
+
+function getLocalStorageLastIndex() {
+    for (let localStorageIndex=1; localStorageIndex=localStorage.length; localStorageIndex++) {
+        localStorageIndex++;
+        return localStorageIndex;
+    } 
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadLocalStorage();
+});
