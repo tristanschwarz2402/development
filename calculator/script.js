637d05c2e82a30a680e8e65f2421f7e12041121f
Tristan Schwarz <schwarz.tristans@gmail.com>
2024-03-01 19:23:13 +0100

    Grundrechnungsarten funktionieren

--- calculator/script.js
+++ calculator/script.js
@@ -24,25 +24,59 @@ window.doCalculate = function () {
     storeNumber(number);
 
     let i = 0;
+    let result = 0;
+
+    // console.log(numbersToCalculate);
 
     if (operationToCalculate === "+") {
         // console.log('addieren');
-        let result = 0;
-
         while (i <= numbersToCalculate.length - 1) {
             result += numbersToCalculate[i];
             i++;
         }
-
-        console.log(result);
-
     } else if (operationToCalculate === "-") {
         // console.log('subtrahieren');
+        while (i <= numbersToCalculate.length - 1) {
+            if (i === 0) {
+                result = numbersToCalculate[i];
+            } else {
+                result -= numbersToCalculate[i];
+            }
+            i++;
+        }
     } else if (operationToCalculate === "*") {
         // console.log('multiplizieren');
+        while (i <= numbersToCalculate.length - 1) {
+            if (i === 0) {
+                result = numbersToCalculate[i];
+            } else {
+                result *= numbersToCalculate[i];
+            }
+            i++;
+        }
     } else {
         // console.log('dividieren');
-    } 
+        while (i <= numbersToCalculate.length - 1) {
+            if (i === 0) {
+                result = numbersToCalculate[i];
+            } else {
+                result /= numbersToCalculate[i];
+            }
+            i++;
+        }
+    }
+    document.getElementById("showNumbers").innerHTML = result;
+}
+
+window.clearAll = function () {
+    document.getElementById("showNumbers").innerHTML = "";
+    numbersToCalculate = [];
+    operationToCalculate = "";
+    alert("clear All");
+}
+
+window.clearCurrent = function () {
+    document.getElementById("showNumbers").innerHTML = "";
 }
 
 // erstellen einer globalen Variable um meine Zahlen darin zu speichern
